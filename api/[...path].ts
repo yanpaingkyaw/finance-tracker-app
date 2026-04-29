07c45b44adce660613616eff945ba8ebb657c2be
Yan Paing Kyaw <yanpaingkyaw@gmail.com>
2026-04-29 15:44:48 +0630

    Fix Vercel API routing

--- api/[...path].ts
+++ api/[...path].ts
@@ -5,8 +5,6 @@ module.exports = async (req, res) => {
     appPromise = import("../server/dist/app.js").then((mod) => mod.default || mod.app);
   }
 
-  req.url = req.url.replace(/^\/api/, "");
-
   const app = await appPromise;
   return app(req, res);
 };
